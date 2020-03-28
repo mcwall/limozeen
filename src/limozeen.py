Scaling fixes and virtical distortion calculation to be used for detection

src/limozeen.py
@@ -1,7 +1,6 @@
 import numpy as np
 import cv2 as cv
-import time
-
+import time, math
 
 def load_video_file():
     cap = cv.VideoCapture('../content/metal.mp4')
@@ -41,21 +40,32 @@ def get_fret_coords(frame):
     
 
 def get_transformed_img(frame, coords):
-    fret_img = np.copy(frame)
+    frameHeight, frameWidth, ch = frame.shape
     topLeft = coords[0]
+    topRight = coords[1]
     bottomLeft = coords[3]
     bottomRight = coords[2]
 
-    print(bottomLeft[0], bottomRight[0])
+    width = bottomRight[0] - bottomLeft[0] + topRight[0] - topLeft[0]
+    width = int(width / 2)
+    height = int(math.sqrt(math.pow(bottomLeft[1] - topLeft[1], 2) + math.pow((bottomRight[0] - topRight[0]), 2))) + heightExp
+    #height = frameHeight + heightExp
 
-    return fret_img[topLeft[1]:bottomLeft[1], bottomLeft[0]:bottomRight[0]]
+    in_coords = np.float32(coords)
+    out_coords = np.float32([[0,0], [width,0], [width,height], [0,height]])
+    M = cv.getPerspectiveTransform(in_coords, out_coords)
+    # M, status = cv.findHomography(in_coords, out_coords)
+    warped_img = cv.warpPerspective(frame, M, (width, frameHeight))
+
+    return warped_img
+
 
 def output(frame, fret):
-    (height, frameWidth, _) = frame.shape
-    (fretHeight, fretWidth, _) = fret.shape
+    height, frameWidth, ch = frame.shape
+    fretHeight, fretWidth, ch = fret.shape
     width = int((frameWidth + fretWidth) * 1.1)
 
-    output = np.zeros((height, width, 3), np.uint8)
+    output = np.zeros((int(height), width, 3), np.uint8)
     output[0:height, 0:frameWidth] = frame
     output[0:fretHeight, width-fretWidth:width] = fret
 
@@ -79,9 +89,9 @@ def process_frame(frame):
 
 def transform_video_file():
     # init stream and window
-    cap = cv.VideoCapture('../content/metal.mp4')
+    cap = cv.VideoCapture('../content/metal1080.mp4')
     cv.namedWindow('vidstream', cv.WINDOW_NORMAL)
-    cv.resizeWindow('vidstream', 1280, 720)
+    cv.resizeWindow('vidstream', 1920, 720)
 
     paused = False
     while cap.isOpened():
@@ -94,7 +104,7 @@ def transform_video_file():
         if key_press & 0xFF == ord('p'):
             paused = not paused
             #cv.imwrite('../content/screenshot.png',frame)
-
+        
         if paused:
             continue
 
@@ -102,25 +112,40 @@ def transform_video_file():
             print("Can't receive frame (stream end?). Exiting ...")
             break
         
-        process_frame(frame)
-        cv.imshow('vidstream', frame)
-
-        #time.sleep(0.0012)
+        cv.imshow('vidstream', process_frame(frame))
 
     cap.release()
     cv.destroyAllWindows()
 
+
 def transform_img():
+    global heightExp
     frame = cv.imread('../content/screenshot.png',cv.IMREAD_UNCHANGED)
     cv.namedWindow('vidstream', cv.WINDOW_NORMAL)
     cv.resizeWindow('vidstream', 1920, 720)
-    
-    img = process_frame(frame)
-    cv.imshow('vidstream', img)    
 
-    cv.waitKey()
+    while True:
+        # check for input
+        key_press = cv.waitKey(1)
+        if key_press & 0xFF == ord('q'):
+            break
+        if key_press & 0xFF == ord('o'):
+            heightExp += 1
+        if key_press & 0xFF == ord('l'):
+            heightExp -= 1
+
+        cv.imshow('vidstream', process_frame(frame))
+
     cv.destroyAllWindows()
 
+heightExp = 0
+
+# Notes are distorted vertically; as the distance from top increases, note height decreases linearly
+# Top note height has been measured to be 12% of total fret board size at the top, with a scaling factor of 1/12
+# In other words note height shrinks by 1 pixel for every 12 pixels a note travels down the screen
+# Rendered note height can thus be calculated as: renderedNoteHeigh = baseNoteHeight - (y * noteDistortion)
+baseNoteHeight = 12
+noteDistortion = 1 / 12
 
 transform_img()
-
+# transform_video_file()