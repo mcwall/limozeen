24466b2a398c4895c6e10e45c6b8eeb605734b25
Matt Wallick <mcwallick@gmail.com>
2020-03-28 23:50:23 -0400

    Support for multiple detection rows; tuned detection filters a little better

--- src/limozeen.py
+++ src/limozeen.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2 as cv
-import time, math, copy
+import time, math, copy, uuid, sys
 
 def load_video_file():
     cap = cv.VideoCapture('../content/metal.mp4')
@@ -62,37 +62,74 @@ def get_transformed_img(frame, coords):
 
 def filter_func(img):
     # output = cv.Sobel(img,cv.CV_64F,0,1,ksize=5)
+    global filter_grayscale, filter_type, filter_threshold
 
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    ret,thresh1 = cv.threshold(gray,127,255,cv.THRESH_BINARY)
-    output = cv.cvtColor(thresh1,cv.COLOR_GRAY2RGB)
+    if filter_grayscale:
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        ret, thresh = cv.threshold(gray, filter_threshold, 255, filter_type)
+        output = cv.cvtColor(thresh, cv.COLOR_GRAY2RGB)
+    else:
+        ret, output = cv.threshold(img, filter_threshold, 255, filter_type)
 
     return output
 
 
-def detect_filter(fret, zone):
+# Find a better implementation for this
+def calculate_match_confidence(img):
+    height, width, ch = img.shape
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    
+    match_confidence = cv.countNonZero(gray) / (width * height)
+
+    return match_confidence
+
+
+def detection_filter(fret, zone):
     #kernel = np.ones((5,5),np.float32)/25
     #output = cv.filter2D(img,-1,kernel)
-    
+
     img = fret[zone.p1[1]:zone.p2[1], zone.p1[0]:zone.p2[0]]
-    fret[zone.p1[1]:zone.p2[1], zone.p1[0]:zone.p2[0]] = filter_func(img)
-    cv.rectangle(fret, zone.p1, zone.p2, (0,0,255), 3)  
+    filtered_image = filter_func(img)
+    fret[zone.p1[1]:zone.p2[1], zone.p1[0]:zone.p2[0]] = filtered_image
+
+    zone.match_confidence = calculate_match_confidence(filtered_image)
 
 
 def output(frame, fret, detection_rows):
+    global font, colors
+
+    # Initialize canvas
     height, frameWidth, ch = frame.shape
     fretHeight, fretWidth, ch = fret.shape
-    width = int((frameWidth + fretWidth) * 1.5)
-
-    for detection_row in detection_rows:
-        for zone in detection_row.zones:
-            detect_filter(fret, zone)
-
+    width = int((frameWidth + fretWidth) * 1.4)
     output = np.zeros((int(height), width, 3), np.uint8)
-    output[0:height, 0:frameWidth] = frame
-    output[0:fretHeight, width-fretWidth:width] = fret
-    output[0:fretHeight, width-2*fretWidth:width-fretWidth] = filter_func(fret)
 
+    # Draw video frame
+    output[0:height, 0:frameWidth] = frame
+
+    # Draw detection zones on fret projections
+    detection_bottom = height - 5
+    detection_top = fretHeight
+    for i, detection_row in enumerate(detection_rows):
+        x = width - (len(detection_rows) - i) * fretWidth
+        fret_copy = np.copy(fret)
+        for idxZone, zone in enumerate(detection_row.zones):
+            detection_filter(fret_copy, zone)
+            cv.rectangle(fret_copy, zone.p1, zone.p2, (0,0,255), 3)
+
+            x1 = int(x + (idxZone / 5 * fretWidth))
+            x2 = int(x1 + fretWidth / 5)
+
+            bar_top = detection_top + 35
+            bar_bottom = detection_bottom - 35
+            bar_height = bar_bottom - bar_top
+            y = int(bar_bottom - zone.match_confidence * bar_height)
+
+            cv.rectangle(output, (x1+2, y), (x2-2, bar_bottom), colors[zone.i], cv.FILLED)
+            cv.putText(output, str(int(100*zone.match_confidence)), (x1, detection_bottom), font, 1, colors[zone.i], 2, cv.LINE_AA)
+
+        # Draw fret projection
+        output[0:fretHeight, x: x+fretWidth] = fret_copy
 
     return output
 
@@ -103,9 +140,10 @@ class DetectionZone:
         x1 = fretWidth * (i / 5)
         x2 = x1 + (fretWidth / 5)
 
+        self.i = i
         self.p1 = (int(x1), top)
         self.p2 = (int(x2), bottom)
-        self.detected = i == 1 or i == 3
+        self.match_confidence = 0.0
 
 
 # Detection zones are represented as a percentage of overall fret height
@@ -122,7 +160,7 @@ class DetectionRow:
 
 
 def detect(fret):
-    detection_rows = [DetectionRow(fret, experiment)]
+    detection_rows = [DetectionRow(fret, detection_height[0]), DetectionRow(fret, detection_height[1])]
 
     return detection_rows
 
@@ -146,67 +184,80 @@ def process_frame(frame):
     return output(frame, fret, detection_rows)
 
 
-def transform_video_file():
+def run(fname):
     # init stream and window
-    cap = cv.VideoCapture('../content/flames720.mp4')
+    
     cv.namedWindow('vidstream', cv.WINDOW_NORMAL)
     cv.resizeWindow('vidstream', 1920, 720)
 
-    paused = False
-    frame = None
-    ret = None
-    while cap.isOpened():
-        global experiment
+    is_video = ".mp4" in fname
+    if is_video:
+        frame = None
+        cap = cv.VideoCapture(fname)
+    else:
+        frame = cv.imread(fname, cv.IMREAD_UNCHANGED)
+        cap = None
 
-        if not paused:
+    paused = False
+    ret = None
+    while True:
+        global detection_height, filter_threshold, filter_grayscale
+
+        if is_video and not paused:
             ret, frame = cap.read()
 
-        # check for input
+        # Flow
         key_press = cv.waitKey(1)
         if key_press & 0xFF == ord('q'):
             break
         if key_press & 0xFF == ord('p'):
             paused = not paused
-            #cv.imwrite('../content/screenshot.png',frame)
-        if key_press & 0xFF == ord('o'):
-            experiment -= 0.005
-        if key_press & 0xFF == ord('l'):
-            experiment += 0.005
-        if key_press & 0xFF == ord('f'):
+        if key_press & 0xFF == ord('f') and is_video:
             ret, frame = cap.read()
             paused = True
         
-        if not ret:
+        # Detection heights
+        if key_press & 0xFF == ord('i'):
+            detection_height[0] -= 0.005
+        if key_press & 0xFF == ord('k'):
+            detection_height[0] += 0.005
+        if key_press & 0xFF == ord('o'):
+            detection_height[1] -= 0.005
+        if key_press & 0xFF == ord('l'):
+            detection_height[1] += 0.005
+
+        # Detection settings
+        if key_press & 0xFF == ord('t'):
+            filter_threshold += 2
+            print(filter_threshold)
+        if key_press & 0xFF == ord('g'):
+            filter_threshold -= 2
+            print(filter_threshold)
+        if key_press & 0xFF == ord('c'):
+            filter_grayscale = not filter_grayscale
+        
+        if key_press & 0xFF == ord('s') and is_video:
+            cv.imwrite('../content/screenshot_' + str(uuid.uuid1()) + '.png', frame)
+        
+        if is_video and (not ret or not cap.isOpened()):
             print("Can't receive frame (stream end?). Exiting ...")
             break
         
         cv.imshow('vidstream', process_frame(frame))
 
-    cap.release()
-    cv.destroyAllWindows()
-
-
-def transform_img():
-    global experiment
-    frame = cv.imread('../content/screenshot.png',cv.IMREAD_UNCHANGED)
-    cv.namedWindow('vidstream', cv.WINDOW_NORMAL)
-    cv.resizeWindow('vidstream', 1920, 720)
-
-    while True:
-        # check for input
-        key_press = cv.waitKey(1)
-        if key_press & 0xFF == ord('q'):
-            break
-        if key_press & 0xFF == ord('o'):
-            experiment -= 0.005
-        if key_press & 0xFF == ord('l'):
-            experiment += 0.005
-
-        cv.imshow('vidstream', process_frame(frame))
+    if cap:
+        cap.release()
 
     cv.destroyAllWindows()
 
-experiment = 0.2
+
+detection_height = [0.25, 0.5]
+
+filter_grayscale = False
+filter_type = cv.THRESH_TOZERO
+filter_threshold = 72
+
+font = cv.FONT_HERSHEY_SIMPLEX
 
 # Notes are distorted vertically; as the distance from top increases, note height decreases linearly
 # Top note height has been measured to be 12% of total fret board size at the top, with a scaling factor of 1/12
@@ -216,5 +267,15 @@ baseNoteHeight = 0.12
 noteDistortion = 1 / 12
 
 
-#transform_img()
-transform_video_file()
+GREEN = (50, 255, 50)
+RED = (50, 50, 255)
+YELLOW = (70, 255, 255)
+BLUE = (255, 180, 0)
+ORANGE = (50, 130, 200)
+
+
+colors = [GREEN, RED, YELLOW, BLUE, ORANGE]
+
+
+fname = sys.argv[1] if len(sys.argv) > 1 else '../content/flames720.mp4'
+run(fname)
